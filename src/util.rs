ab54fedb28705fd60f31ad9b7acb43da021d0613
Andrew Poelstra <apoelstra@wpsoftware.net>
2025-08-25 00:10:11 +0000

    delete merkle.rs and unused supporting code
    `mod merkle` was removed in 24b11afb3663600851f0d591fca54dc17c7e95df
    but the file it refers to was left hanging around. This results in
    an unused code warning.
    
    With this change, I can run a clean
    
    cargo +nightly clippy --all-targets --workspace --all-features --all -- -D warnings

--- src/util.rs
+++ src/util.rs
@@ -1,11 +1,8 @@
-use std::fmt;
-
 use elements::hashes::{sha256, Hash};
 use elements::secp256k1_zkp as secp256k1;
 use secp256k1::rand::{self, Rng, SeedableRng};
 use simplicity::dag::{DagLike, MaxSharing, NoSharing};
 use simplicity::jet::Elements;
-use simplicity::node::Inner;
 use simplicity::{node, RedeemNode};
 use simplicityhl::num::U256;
 use simplicityhl::simplicity::Preimage32;
@@ -113,43 +110,6 @@ pub fn get_compression_factor<M: node::Marker>(node: &node::Node<M>) -> usize {
     debug_assert!(0 < shared_len);
     debug_assert!(shared_len <= unshared_len);
     unshared_len / shared_len
-}
-
-pub struct DisplayInner<'a, M: node::Marker>(&'a node::Node<M>);
-
-impl<'a, M: node::Marker> From<&'a node::Node<M>> for DisplayInner<'a, M> {
-    fn from(node: &'a node::Node<M>) -> Self {
-        Self(node)
-    }
-}
-
-impl<M: node::Marker> fmt::Display for DisplayInner<'_, M> {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self.0.inner() {
-            Inner::Iden => f.write_str("iden"),
-            Inner::Unit => f.write_str("unit"),
-            Inner::InjL(_) => f.write_str("injl"),
-            Inner::InjR(_) => f.write_str("injr"),
-            Inner::Take(_) => f.write_str("take"),
-            Inner::Drop(_) => f.write_str("drop"),
-            Inner::Comp(_, _) => f.write_str("comp"),
-            Inner::Case(_, _) => f.write_str("case"),
-            Inner::AssertL(_, _) => f.write_str("assertl"),
-            Inner::AssertR(_, _) => f.write_str("assertr"),
-            Inner::Pair(_, _) => f.write_str("pair"),
-            Inner::Disconnect(_, _) => f.write_str("disconnect"),
-            Inner::Witness(_) => f.write_str("witness"),
-            Inner::Fail(_) => f.write_str("fail"),
-            Inner::Jet(jet) => write!(f, "jet_{jet}"),
-            Inner::Word(value) => write!(f, "const {value}"),
-        }
-    }
-}
-
-impl<M: node::Marker> fmt::Debug for DisplayInner<'_, M> {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        fmt::Display::fmt(self, f)
-    }
 }
 
 fn unspendable_internal_key() -> secp256k1::XOnlyPublicKey {
